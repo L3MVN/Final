569b0775443ef745778436245649b9c5d73842d7
L3MVN <llyons305@gmail.com>
2024-06-12 19:58:41 -0700

    Updated from HW08, Completed HW09, created _list, _form,_detail,_confirm_delete. And all the cool things.

--- Hw09/guitars/urls.py
+++ Hw09/guitars/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required, permission_required
 from . import views
+
+app_name = "guitars"
 
 urlpatterns = [
     # Empty (root) path goes to the index view
-    path('', views.index, name='home'),
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("about", views.about, name="about"),
+    path('', views.DepartmentListView.as_view(), name='home'),
+    path("<int:pk>", views.DepartmentDetailView.as_view(), name="detail"),
+    path("add", permission_required('guitars.add_guitar')(views.DepartmentCreateView.as_view()), name="create"),
+    path("<int:pk>/update", permission_required('guitars.change_guitar')(views.DepartmentUpdateView.as_view()), name="update"),
+    path("<int:pk>/delete", permission_required('guitars.delete_guitar')(views.DepartmentDeleteView.as_view()), name="delete"),
 ]
--- Hw09/guitars/views.py
+++ Hw09/guitars/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render
-#from django.views.generic import ListView, DetailView
-#from Django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Department
 
 # Create your views here.
 
 
-def index(request):
-    context = {}
-    context['departments'] = Department.objects.order_by('name','location')[:12]
-    return render(request, 'guitars/index.html', context)
+#def index(request):
+    #context = {}
+    #context['departments'] = Department.objects.order_by('name','location')[:12]
+    #return render(request, 'guitars/index.html', context)
+class DepartmentCreateView(CreateView):
+    model = Department
+    fields = ["name", "location"]
 
-#class DepartmentListView(ListView):
-#    model = Department
+class DepartmentListView(ListView):
+    model = Department
+
+class DepartmentDetailView(DetailView):
+    model = Department
+
+class DepartmentUpdateView(UpdateView):
+    model = Department
+    fields = ["name","location"]
+
+class DepartmentDeleteView(DeleteView):
+    model = Department
+    success_url = "/"
 
 def about(request):
     context = {}
     return render(request, 'guitars/about.html', context)
+
